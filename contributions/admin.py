32f374aaa7278f27305648cd6cbbbe77ce712ffa
root <selwak@gmail.com>
2014-01-31 18:13:48 +0100

    additional otopns for admin in contributions

--- contributions/admin.py
+++ contributions/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from models import ProductContribution, Sponsorship, Project
 
-admin.site.register(ProductContribution)
+class ProductContributionAdmin(admin.ModelAdmin):
+    list_display = ('product_id', 'persona_id', 'role')
+    list_filter = ('role',)
+    search_fields = ['product_id__title', 'product_id__ean', 'persona_id__name_surname']
+    
+admin.site.register(ProductContribution, ProductContributionAdmin)
 admin.site.register(Sponsorship)
 admin.site.register(Project)

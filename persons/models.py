a4c37908e166e05f8662e257bd55c7006f076199
root <selwak@gmail.com>
2014-01-31 18:15:04 +0100

    cosmetic string changes in persons/models.py

--- persons/models.py
+++ persons/models.py
@@ -9,7 +9,7 @@ class Persona(models.Model):
     name_surname = models.CharField(_(u'name'), max_length=32)
     email = models.EmailField()
     tel = models.CharField(
-        _(u'name'), max_length=32,
+        _(u'tel'), max_length=32,
         blank=True, null=True)
     adress = models.TextField()
     country = models.CharField(max_length=50)
@@ -17,7 +17,7 @@ class Persona(models.Model):
     observations = models.TextField()
 
     def __unicode__(self):
-        return "Audio Download %s" % (self.name_surname)
+        return self.name_surname
 
 
 class Author(models.Model):
@@ -32,7 +32,7 @@ class Author(models.Model):
         verbose_name_plural = _(u"authors")
 
     def __unicode__(self):
-        return "%s - %s" % (self.name, self.surname)
+        return "%s %s" % (self.name, self.surname)
 
     @models.permalink
     def get_absolute_url(self):

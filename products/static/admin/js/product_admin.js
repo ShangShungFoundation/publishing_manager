65b082acf77b4c0bd8cd59d3be63c09d988e1a0d
root <selwak@gmail.com>
2014-01-31 18:22:06 +0100

    make sure js dont hide conributionProject inlines

--- products/static/admin/js/product_admin.js
+++ products/static/admin/js/product_admin.js
@@ -7,13 +7,13 @@ function toogle_type(support_type){
 		django.jQuery(".inline-group").hide();
 		django.jQuery("#"+support_type+"_set-group").show();
 	}
+	django.jQuery("#productcontribution_set-group").show();
 }
 
 django.jQuery(function(){ 
 	var support_type_ele = django.jQuery("#id_support_type")
 	var support_type = support_type_ele.val();
 	toogle_type(support_type);
-
 	support_type_ele.on("change", function(){
 		toogle_type(this.value)
 	})
